2c59fe9bfbf343003f6a9a7cc037c9a9f19436ea
Reshma-10 <reshmahm368@gmail.com>
2025-01-03 12:10:26 +0530

    Update sales_data_analysis_pandas_project.py

--- sales_data_analysis_pandas_project.py
+++ sales_data_analysis_pandas_project.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_excel('/content/superstore_sales.xlsx')
 df.head()
